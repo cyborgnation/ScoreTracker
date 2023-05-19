84e81fb79fc3d90f6251b49e35c14cdcd7e7deb7
Joe barber <iamjoebarber@gmail.com>
2023-05-19 10:46:20 -0500

    Initial Commit

---
+++ ScoreTracker/ScoreTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreTrackerApp.swift
+//  ScoreTracker
+//
+//  Created by Joe Barber on 5/19/23.
+//
+
+import SwiftUI
+
+@main
+struct ScoreTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
