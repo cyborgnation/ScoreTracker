725f8d43796585a6bf4ea05525a7c96fb746c870
Joe barber <iamjoebarber@gmail.com>
2023-05-19 21:37:47 -0500

    Attempting to fix colors

--- ScoreTracker/ContentView.swift
+++ ScoreTracker/ContentView.swift
@@ -10,27 +10,34 @@ struct ContentView: View {
     @EnvironmentObject var gameManager: GameManager
     @State private var newPlayerName = ""
     
-
+    
     var body: some View {
-        VStack {
-            HStack {
-                TextField("Enter player's name...", text: $newPlayerName)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding()
-                Button("Add Player") {
-                    gameManager.addPlayer(name: newPlayerName)
-                    newPlayerName = ""
+        ZStack {
+            Color.black.ignoresSafeArea()
+            
+            NavigationView {
+                VStack {
+                    HStack {
+                        TextField("Enter player's name...", text: $newPlayerName)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .padding()
+                        Button("Add Player") {
+                            gameManager.addPlayer(name: newPlayerName)
+                            newPlayerName = ""
+                        }
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                    }
+                    
+                    ScoreBoardView()
+                        .environmentObject(gameManager)
                 }
-                .padding()
-                .background(Color.blue)
-                .foregroundColor(.white)
-                .cornerRadius(10)
+                .padding(.top)
+                .navigationTitle("Score Keeper")
             }
-
-            ScoreBoardView()
-                .environmentObject(gameManager)
+            .background(Color.clear)
         }
-        .background(Color.black) // <- Add this line here
-        .edgesIgnoringSafeArea(.all) // <- This line makes the color fill the entire screen area
     }
 }
